653b99cbf19b588dd31292349b6c4ed4d04bdf1a
nelsonaq <chibuds217@gmail.com>
2022-04-18 02:27:59 +0800

    Refactored js codes and added some documentation

--- js/destination.js
+++ js/destination.js
@@ -1,7 +1,15 @@
+"use strict";
+
+/**
+ * Navigation bar elements
+ */
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
 
+/**
+ * Elements in the container
+ */
 const planetNames = document.querySelector(".planet-names");
 const planetImg = document.querySelector(".container img");
 const planetInfo = document.querySelector(".planet-info h1");
@@ -9,6 +17,9 @@ const planetDescription = document.querySelector(".planet-info p");
 const planetDistance = document.querySelector(".distance");
 const planetTravel = document.querySelector(".travel");
 
+/**
+ * Function that hides and display the overlay link counter when in mobile view
+ */
 const showLinks = function () {
   overlay.classList.toggle("hide-overlay");
 };
@@ -16,6 +27,11 @@ const showLinks = function () {
 hamburger.addEventListener("click", showLinks);
 close.addEventListener("click", showLinks);
 
+/**
+ * Updates the UI based on the given arguments.
+ * @param {Object} data - Object from the local json file.
+ * @param {Number} planetNum - Data id of selected planet.
+ */
 const updateUI = function (data, planetNum) {
   const planetData = data.destinations[planetNum];
   planetInfo.textContent = planetData.name;
@@ -25,6 +41,10 @@ const updateUI = function (data, planetNum) {
   planetImg.src = `/assets/destination/image-${planetData.name}.png`;
 };
 
+/**
+ * Handles fetching the local data json file.
+ * @returns {Promise/Object} - Promise that contains the data object of the json file.
+ */
 const getJSON = async function () {
   try {
     const response = await fetch("data.json");
@@ -35,18 +55,37 @@ const getJSON = async function () {
   }
 };
 
-planetNames.addEventListener("click", function (e) {
-  e.preventDefault();
-  const selected = e.target.closest("li");
-  if (!selected) return;
-  const selectedId = selected.dataset.id;
+/**
+ * Render the planet animation and reset the animation when switching.
+ */
+const resetPlanetAnimation = function () {
   planetImg.classList.remove("planet-img-animation");
   void planetImg.offsetWidth;
   planetImg.classList.add("planet-img-animation");
+};
 
+/**
+ * Displays UI for selected planet.
+ * @param {Element} selectedPlanet - Selected planet's element.
+ */
+const displaySelectedPlanet = function (selectedPlanet) {
   planetNames
     .querySelectorAll("li")
-    .forEach((planet) => planet.classList.remove("active-planet"));
-  selected.classList.add("active-planet");
+    .forEach((planet) => planet.classList.remove("selected-planet"));
+  selectedPlanet.classList.add("selected-planet");
+};
+
+/**
+ * Handles all the changes that occurs when switching planets.
+ */
+planetNames.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const selected = e.target.closest("li");
+  const selectedId = selected.dataset.id;
+  if (!selected) return;
+
+  resetPlanetAnimation();
+  displaySelectedPlanet(selected);
   getJSON().then((data) => updateUI(data, selectedId));
 });
--- js/home.js
+++ js/home.js
@@ -1,7 +1,15 @@
+"use strict";
+
+/**
+ * Navigation bar elements
+ */
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
 
+/**
+ * Function that hides and display the overlay link counter when in mobile view
+ */
 const showLinks = function () {
   overlay.classList.toggle("hide-overlay");
 };
