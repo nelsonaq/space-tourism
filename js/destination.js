030a9b65de0b62b817ed03e1b1dcba5dc55a08f4
nelsonaq <chibuds217@gmail.com>
2022-04-21 09:36:45 +0800

    added overlay when opening nav drawer

--- js/destination.js
+++ js/destination.js
@@ -5,6 +5,7 @@
  */
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
+const navDrawer = document.querySelector(".nav-drawer");
 const overlay = document.querySelector(".overlay");
 
 /**
@@ -21,11 +22,13 @@ const destinationTravel = document.querySelector(".travel");
  * Function that hides and display the overlay link counter when in mobile view
  */
 const showLinks = function () {
-  overlay.classList.toggle("hide-overlay");
+  navDrawer.classList.toggle("hide-nav-drawer");
+  overlay.classList.toggle("display-overlay");
 };
 
 hamburger.addEventListener("click", showLinks);
 close.addEventListener("click", showLinks);
+overlay.addEventListener("click", showLinks);
 
 /**
  * Updates the UI based on the given arguments.
--- js/home.js
+++ js/home.js
@@ -5,14 +5,17 @@
  */
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
+const navDrawer = document.querySelector(".nav-drawer");
 const overlay = document.querySelector(".overlay");
 
 /**
  * Function that hides and display the overlay link counter when in mobile view
  */
 const showLinks = function () {
-  overlay.classList.toggle("hide-overlay");
+  navDrawer.classList.toggle("hide-nav-drawer");
+  overlay.classList.toggle("display-overlay");
 };
 
 hamburger.addEventListener("click", showLinks);
 close.addEventListener("click", showLinks);
+overlay.addEventListener("click", showLinks);
