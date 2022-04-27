15448e429fe15b84c420032c00c5d33256ad48b6
nelsonaq <chibuds217@gmail.com>
2022-04-27 09:47:50 +0800

    technology mobile page UI

---
+++ js/technology.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Navigation bar elements
+ */
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const navDrawer = document.querySelector(".nav-drawer");
+const overlay = document.querySelector(".overlay");
+
+/**
+ * Function that hides and display the overlay link counter when in mobile view
+ */
+const showLinks = function () {
+  navDrawer.classList.toggle("hide-nav-drawer");
+  overlay.classList.toggle("display-overlay");
+};
+
+hamburger.addEventListener("click", showLinks);
+close.addEventListener("click", showLinks);
+overlay.addEventListener("click", showLinks);
