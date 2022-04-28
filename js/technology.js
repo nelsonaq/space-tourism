85f0a0866a30c7f14d8b6e94759aa5b9d6daf82b
nelsonaq <chibuds217@gmail.com>
2022-04-28 08:25:06 +0800

    refactored css and html code to accomodate switching of technology image url

--- js/technology.js
+++ js/technology.js
@@ -8,6 +8,10 @@ const close = document.querySelector(".close");
 const navDrawer = document.querySelector(".nav-drawer");
 const overlay = document.querySelector(".overlay");
 
+const btnNums = document.querySelector(".btn-nums");
+const techImgPortrait = document.querySelector(".tech-img-portrait");
+const techImgLandscape = document.querySelector(".tech-img-landscape");
+
 /**
  * Function that hides and display the overlay link counter when in mobile view
  */
@@ -19,3 +23,30 @@ const showLinks = function () {
 hamburger.addEventListener("click", showLinks);
 close.addEventListener("click", showLinks);
 overlay.addEventListener("click", showLinks);
+
+const getJSON = async function () {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateUI = function (data, techNum) {
+  const techData = data.technology[techNum];
+  techImgLandscape.style.background = `url(${techData.images.landscape})bottom / cover`;
+  techImgPortrait.style.background = `url(${techData.images.portrait})`;
+};
+
+btnNums.addEventListener("click", function (e) {
+  e.preventDefault();
+  const selected = e.target.closest(".btn-num");
+  //* Guard Clause
+  if (!selected) return;
+  const selectedNum = selected.dataset.num;
+  console.log(selected);
+  console.log(selectedNum);
+  getJSON().then((data) => updateUI(data, selectedNum));
+});
